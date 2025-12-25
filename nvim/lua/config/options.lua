59ec27a875ff64b272a9a413b7b18f972eb78059
fred <fyang151@gmail.com>
2025-12-24 22:20:24 -0800

    Nvim breakpointindent and background.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,10 +3,12 @@ vim.opt.tabstop = 4
 vim.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-    
+
 -- UI
 vim.opt.number = true
 vim.opt.showmode = false
+
+vim.opt.breakindent = true
 
 vim.opt.clipboard = "unnamedplus"
 
